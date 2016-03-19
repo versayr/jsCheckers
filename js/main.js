9777ae70d6958a5ac7f6a51b01cee7f0f22f2da9
Versayr <themoose3@gmail.com>
2016-03-19 13:05:19 -0400

    Added some js

--- js/main.js
+++ js/main.js
@@ -3,4 +3,25 @@
 // -- Pieces Objects
 // Reset Function
 // Undo Function
+// Board Drawing Function:
 // 
+// 
+
+var gameState = {};
+
+gameState.turn = "black";
+gameState.reset = function() {
+
+};
+gameState.undo = function() {
+
+};
+
+var gameBoard = {
+  // Draw the Board
+  // Draw the Rows
+  // Draw the Squares
+  // Squares are Playable/Not
+  // Squares are Occupies/Not
+};
+var men = [];
